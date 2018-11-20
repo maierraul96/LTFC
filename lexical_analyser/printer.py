73ee555e41d6214653e55e039dff9e1f2edd6d24
maierraul <maierraul96@gmail.com>
2018-11-20 17:25:25 +0200

    Printer print list

--- lexical_analyser/printer.py
+++ lexical_analyser/printer.py
@@ -19,3 +19,16 @@ class Printer:
         for row in table:
             print(form.format(list(row.keys())[0], list(row.values())[0]))
         print('-' * (max_len_key + max_len_value + 8))
+
+    @staticmethod
+    def print_list(title, content):
+        max_len = max(max(map(lambda x: len(x), content)),len(title))
+        line = str('-' * (max_len + 6))
+        template = str('|' + '{:^' + str(max_len + 4) + '}|')
+
+        print(line)
+        print(template.format(title))
+        print(line)
+        for element in content:
+            print(template.format(element))
+        print(line)
