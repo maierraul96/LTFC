46467aa7461c5c75fd288b16903306ad4be36190
maierraul <maierraul96@gmail.com>
2018-11-22 00:17:38 +0200

    Complete tests for importing from file

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from lexical_analyser.core.coreAnalyser import *
 from lexical_analyser.printer import Printer
 from lexical_analyser.afMeniu import Menu
 from lexical_analyser.core.finiteAutomate import *
+import unittest
 
 # analyser = AnalyserCore("raza.lbc")
 #
@@ -12,6 +13,9 @@ from lexical_analyser.core.finiteAutomate import *
 # Printer().show(analyser.CODTS, "FIP")
 # print('\n')
 # Printer().show(analyser.NAMES, "TS")
+
+
+# unittest.main()
 
 af = AF()
 af.load_states_from_file("AF.json")
--- tests/finiteAutomate_tests/finiteAutomateTests.py
+++ tests/finiteAutomate_tests/finiteAutomateTests.py
@@ -1,5 +1,5 @@
 import unittest
-from lexical_analyser.core.finiteAutomate import AF, StateType
+from lexical_analyser.core.finiteAutomate import *
 
 
 class JsonImportTest(unittest.TestCase):
@@ -7,9 +7,37 @@ class JsonImportTest(unittest.TestCase):
 
     def setUp(self):
         self.af = AF()
+        self.af.load_states_from_file("resources/test_af.json")
 
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_count_states(self):
+        self.assertEqual(len(self.af.states), 3)
+
+    def test_states(self):
+        self.assertListEqual(list(self.af.states.keys()),
+                             ['start', 'middle', 'end'])
+        self.assertEqual(self.af.states['start'].name, 'start')
+        self.assertEqual(self.af.states['middle'].name, 'middle')
+        self.assertEqual(self.af.states['end'].name, 'end')
+
+    def test_transitions(self):
+        self.assertListEqual(self.af.states['start'].transitions[0].conditions,
+                             ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"])
+        self.assertListEqual(self.af.states['start'].transitions[1].conditions,
+                             ["."])
+        self.assertListEqual(self.af.states['middle'].transitions[0].conditions,
+                             ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"])
+        self.assertListEqual(self.af.states['end'].transitions[0].conditions,
+                             ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"])
+
+        self.assertEqual(self.af.states['start'].transitions[0].next_state, 'start')
+        self.assertEqual(self.af.states['start'].transitions[1].next_state, 'middle')
+        self.assertEqual(self.af.states['middle'].transitions[0].next_state, 'end')
+        self.assertEqual(self.af.states['end'].transitions[0].next_state, 'end')
+
+    def test_setup(self):
+        self.assertEqual(self.af.current_state.name, 'start')
+        self.assertEqual(self.af.max_length, 0)
+        self.assertListEqual(self.af.history, list())
 
 
 if __name__ == '__main__':
